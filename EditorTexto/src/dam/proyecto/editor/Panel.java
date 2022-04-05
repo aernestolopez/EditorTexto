5835393119be7baa2734ef2a5fd91f2bccb22e3c
aernestolopez <aernestolh@gmail.com>
2022-04-05 09:37:01 +0200

    Creación del área de texto
    Para esto necesitaremos las clases JPanel y JTextPanel necesarias para poder crear areas de texto. Tras esto crearemos el metodo que agregara el area de texto al contenedor para finalmente agregar en la pestaña la ventana con el area de texto

--- EditorTexto/src/dam/proyecto/editor/Panel.java
+++ EditorTexto/src/dam/proyecto/editor/Panel.java
@@ -2,6 +2,7 @@ package dam.proyecto.editor;
 
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextPane;
 /**
  * Clase utilizada para crear paneles al marco
  * @author Antonio Lopez
@@ -9,12 +10,31 @@ import javax.swing.JTabbedPane;
  */
 public class Panel extends JPanel{
 	public Panel() {
-
+		//---------------Area de Texto-------------------
 		tPane =new JTabbedPane();
 	
+		//-----------------------------------------------
+		CreaPanel();
+		
 		
 		add(tPane);
 	}
+	//Metodo usado para crear una pestaña con un area de texto 
+	public void CreaPanel() {
+		//instanciamos el panel
+		ventana=new JPanel();
+		//instanciamos el area de texto
+		areaTexto=new JTextPane();
+		//Agregamos el area de texto en el contenedor
+		ventana.add(areaTexto);
+		//Agregamos la ventana que tiene en su interior el area de texto en la pestaña
+		tPane.addTab("title", ventana);
+	}
+	
+	
 	//Clase usada para poder crear pestañas en nuestro editor
 	private JTabbedPane tPane;
+	//Clases utilizadas para poder crear areas de texto
+	private JPanel ventana;
+	private JTextPane areaTexto;
 }
